bfa38949736ba6aaee26d0cf8f197f1138295e41
LeyouJcmu <leyouj@andrew/cmu/edu>
2026-02-24 10:06:45 -0500

    done

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,11 +12,29 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("andrew id")) {
-    return "leyouj"
+    return "leyouj";
   }
 
   if (query.toLowerCase().includes("andrewid")) {
-    return "leyouj"
+    return "leyouj";
+  }
+
+  if (query.toLowerCase().includes("Which of the following numbers is the largest")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers) {
+      const intNumbers = numbers.map((n: string) => parseInt(n, 10));
+      return Math.max(...intNumbers).toString();
+    }
+    return "No numbers found in the query string";
+  }
+
+  if (query.toLowerCase().includes("plus")) {
+    const numbers = query.match(/\d+/g);
+    if (numbers) {
+      const intNumbers = numbers.map((n: string) => parseInt(n, 10));
+      return intNumbers.reduce((a: number, b: number) => a + b, 0).toString();
+    }
+    return "No numbers found in the query string";
   }
 
   return "";
